c6cf4987fc015db362f1ff81fdbaa8da9bc7efba
한승룡 <nofaterock@naver.com>
2019-04-22 15:07:22 +0900

    stateful

--- quartz/src/main/java/com/nofaterock/quartz/springquartz/SpringQuartzJob2.java
+++ quartz/src/main/java/com/nofaterock/quartz/springquartz/SpringQuartzJob2.java
@@ -1,21 +1,32 @@
 package com.nofaterock.quartz.springquartz;
 
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.quartz.PersistJobDataAfterExecution;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
 
 /**
+ * Stateful Job
+ *
  * @author 한승룡
  * @since 2019-04-12
  */
 @Slf4j
-public class SpringQuartzJob2 extends QuartzJobBean {
+@PersistJobDataAfterExecution
+@DisallowConcurrentExecution
+public class SpringQuartzJob2 extends MethodInvokingJobDetailFactoryBean.MethodInvokingJob {
 
 	public static final String JOB_NAME = "SpringQuartzJob2";
 
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		log.info("{} : Hello {}", JOB_NAME, context.getJobDetail().getJobDataMap().get("name").toString());
 	}
 }
